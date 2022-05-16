e4cf4762cab18e9f36d58976b79eddd6cf137865
Calum Cook <calumcook@Calums-Air-2.broadband>
2022-05-16 20:14:56 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,13 @@
+cookies_eaten = input("How many cookies have you had?: ")
+
+if cookies_eaten == "0":
+    print("Have a cookie!")
+
+elif cookies_eaten <= "3":
+    print("Have another cookie!")
+
+elif cookies_eaten == "4":
+    print("Whoa there...")
+
+elif cookies_eaten >= "5":
+    print("SHAME!!!!!")
